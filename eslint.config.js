2b1d073931f7d44f0997371105c495d16f977ecd
Celtian <dominik.hladik@seznam.cz>
2026-08-02 21:09:22 +0200

    feat(boundaries): boundaries added

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
 // @ts-check
 const eslint = require('@eslint/js');
 const { defineConfig } = require('eslint/config');
+const boundaries = /** @type {import('eslint').ESLint.Plugin} */ (
+  require('eslint-plugin-boundaries')
+);
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 
@@ -14,7 +17,39 @@ module.exports = defineConfig([
       angular.configs.tsRecommended,
     ],
     processor: angular.processInlineTemplates,
+    plugins: {
+      boundaries,
+    },
+    settings: {
+      'boundaries/root-path': __dirname,
+      'boundaries/elements': [
+        {
+          type: 'electron',
+          pattern: 'projects/electron',
+          partialMatch: false,
+        },
+        {
+          type: 'docs',
+          pattern: 'projects/docs',
+          partialMatch: false,
+        },
+      ],
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
     rules: {
+      'boundaries/dependencies': [
+        'error',
+        {
+          default: 'disallow',
+          checkAllOrigins: false,
+          checkUnknownLocals: false,
+          checkInternals: false,
+        },
+      ],
       '@angular-eslint/directive-selector': [
         'error',
         {
